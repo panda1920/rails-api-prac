bc27d059385cdfd25442afe5994dadec9b36d53c
Kei Kamigama@Desktop <15102478+panda1920@users.noreply.github.com>
2021-09-13 08:47:30 +0900

    Enabled SSL for frontend.
    - Changed CORS settings for api
    - Created new conf file for nginx that adds server directory for ssl
    - updated gitignore to ignore certificate files
    - settings in docker-compose to mount certificate and conf on nginx

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3004'
+    origins [ 'http://localhost:3004', 'https://localhost:3003' ]
 
     resource '*',
       headers: :any,
