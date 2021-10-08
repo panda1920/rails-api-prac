545fbef40fc913508d3251d27179b94598607605
Kei Kamigama@Desktop <15102478+panda1920@users.noreply.github.com>
2021-10-08 13:04:27 +0900

    Getting mail previews to work

--- test/mailers/previews/prac_mailer_preview.rb
+++ test/mailers/previews/prac_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/prac_mailer
 class PracMailerPreview < ActionMailer::Preview
+  def welcome_email
+    PracMailer.welcome_email
+  end
 
+  def studio_mail
+    PracMailer.studio_mail
+  end
 end
