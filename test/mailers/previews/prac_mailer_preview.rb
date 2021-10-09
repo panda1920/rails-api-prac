c065f09f53b9391b99e02dc079001ffb0e7be273
Kei Kamigama@Desktop <15102478+panda1920@users.noreply.github.com>
2021-10-09 16:05:38 +0900

    Broke up existing email into layout and embedded view

--- test/mailers/previews/prac_mailer_preview.rb
+++ test/mailers/previews/prac_mailer_preview.rb
@@ -1,6 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/prac_mailer
 class PracMailerPreview < ActionMailer::Preview
-  def welcome_email
+  
+  # method name can be arbitrary
+  # does not have to correspond to the underlying mailer methods
+  def welcome1
     PracMailer.welcome_email
   end
 
